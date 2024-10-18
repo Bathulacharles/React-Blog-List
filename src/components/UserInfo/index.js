e62302e2d55370eff82aab807cdd6fd0facb4da8
Bathulacharles <charles.bathula22@gmail.com>
2024-10-18 09:39:11 +0000

    React-Blog-List

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="img-profile"
+    />
+    <h1 className="name-heading">Wade Warren</h1>
+    <p className="description">Software developer at Uk</p>
+  </div>
+)
+export default UserInfo
